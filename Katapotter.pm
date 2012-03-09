10d5bcf988e43db27d0700560447df597038f39d
BibLibre Team <pnom@biblibre.com>
2012-03-09 14:24:24 +0100

    4th test passed

--- Katapotter.pm
+++ Katapotter.pm
@@ -3,14 +3,21 @@ package Katapotter;
 use Modern::Perl;
 
 sub price {
-    my @booklist=@_;
-    my $nb_books=scalar(@booklist);
+    my @booklist = @_;
+    my $nb_books = scalar(@booklist);
+    my $unit_price = 8;
     my $remise = {
-    0 => 0,
-    1 => 1,
-    2 => 0.95,
-    3 => 0.90
+        0 => 0,
+        1 => 1,
+        2 => 0.95,
+        3 => 0.90
     };
-    return $nb_books*8*$remise->{$nb_books}
+    my $price;
+    if($nb_books == 2 and $booklist[0] == $booklist[1]) {
+	$price = 2*$unit_price;
+    } else {
+        $price = $nb_books*$unit_price*$remise->{$nb_books};
+    }
+    return $price;
 }
 1;
--- katapotter.t
+++ katapotter.t
@@ -3,9 +3,10 @@ use Modern::Perl;
 use Test::More;
 use Katapotter;
 
-my $nb_test = 3;
+my $nb_test = 4;
 plan tests => $nb_test;
 
 is(Katapotter::price(1), 8, "1 book = 8€");
 is(Katapotter::price(1,2), 8*2*0.95, "2 books differents");
 is(Katapotter::price(1,2,3), 8*3*0.90, "3 books differents");
+is(Katapotter::price(1,1), 8*2, "2 identical books");
