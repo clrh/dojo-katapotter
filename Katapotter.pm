69c479fad7e72434582172618cbf9ae938c68f61
Claire Hernandez <claire.hernandez@biblibre.com>
2012-04-27 14:46:35 +0200

    All tests successful + refactoring

--- Katapotter.pm
+++ Katapotter.pm
@@ -5,23 +5,30 @@ use List::MoreUtils qw/uniq/;
 
 sub price {
     my @booklist = @_;
-    my $nb_books = scalar(@booklist);
     my $unit_price = 8;
     my $remise = {
         0 => 0,
         1 => 1,
         2 => 0.95,
-        3 => 0.90
+        3 => 0.90,
+        4 => 0.8
     };
     my $price;
-    my $unique_books=scalar(uniq(@booklist)) ;
-    if($nb_books == 2 and $booklist[0] == $booklist[1]) {
-	$price = 2*$unit_price;
-    } elsif (($nb_books == 3 ) and ($unique_books == 2)) {
-          $price = $unique_books*$unit_price*$remise->{$unique_books}+$unit_price;
-    } else {
-	$price = $nb_books*$unit_price*$remise->{$nb_books};
+    my @liste_unique;
+    my @liste_restants;
+
+    if(scalar @booklist == 0){
+        return 0;
     }
+
+    foreach my $book_element (@booklist) {
+        if ( not grep /$book_element/,  @liste_unique ) { 
+            push @liste_unique, $book_element;
+        } else {
+            push @liste_restants, $book_element;
+        }
+    }
+    $price = @liste_unique * $unit_price * $remise->{scalar(@liste_unique)} + price(@liste_restants);
     return $price;
 }
 1;
--- katapotter.t
+++ katapotter.t
@@ -3,11 +3,18 @@ use Modern::Perl;
 use Test::More;
 use Katapotter;
 
-my $nb_test = 5;
-plan tests => $nb_test;
 
 is(Katapotter::price(1), 8, "1 book = 8€");
 is(Katapotter::price(1,2), 8*2*0.95, "2 books differents");
 is(Katapotter::price(1,2,3), 8*3*0.90, "3 books differents");
 is(Katapotter::price(1,1), 8*2, "2 identical books");
 is(Katapotter::price(1,1,2), 8*2*0.95+8, "2 identical books and one book");
+is(Katapotter::price(1,2,3,4), 8*4*0.8, "4 different bookss");
+
+done_testing;
+
+#(1,2,3,4,5) = 5 * 8 * 0.75
+#(1,2,3,1) = 8 * 3 * 0.9 + 8
+#(1,1,2,2,3,3,4,5) = (1,2,3,4) + (1,2,3,5)
+#(1,1,2,2,3) = (1,2,3) + (1,2)
+
